58fc29056fafcf5abb3d035b36ba96625567d544
Christopher Bernhard <58096490+cbernhard21@users.noreply.github.com>
2022-03-20 09:24:23 -0500

    Add files via upload

--- backend/controllers/leagueController.js
+++ backend/controllers/leagueController.js
@@ -1,35 +1,42 @@
+const asyncHandler = require('express-async-handler');
+
+
 // @desc get league info
 // @route GET /api/ejffl
 // @access Private
-const getLeagueInfo = (req, res) => {
-    res.status(200).json({message: 'get league data'})
-}
+const getLeagueInfo = asyncHandler(async(req, res) => {
+  res.status(200).json({ message: 'get league data' })
+});
 
 // @desc set league info
 // @route POST /api/ejffl
 // @access Private
-const setLeagueInfo = (req, res) => {
-    console.log(req.body);
-    res.status(200).json({message: 'Create league info'})
-}
+const setLeagueInfo = asyncHandler(async(req, res) => {
+  if (!req.body.teamName) {
+    res.status(400);
+    throw new Error('Please Enter A Team Name');
+  }
+  console.log(req.body)
+  res.status(200).json({ message: 'Create league info' });
+})
 
 // @desc update league info
 // @route PUT /api/ejffl/:id
 // @access Private
-const updateLeagueInfo = (req, res) => {
-    res.status(200).json({message: `update league info ${req.params.id}`});
-}
+const updateLeagueInfo = asyncHandler(async(req, res) => {
+  res.status(200).json({ message: `update league info ${req.params.id}` });
+})
 
 // @desc delete league info
 // @route DELETE /api/ejffl:id
 // @access Private
-const deleteLeagueInfo = (req, res) => {
-    res.status(200).json({message: `delete league info ${req.params.id}`});
-}
+const deleteLeagueInfo = asyncHandler(async(req, res) => {
+  res.status(200).json({ message: `delete league info ${req.params.id}` });
+})
 
 module.exports = {
-    getLeagueInfo,
-    setLeagueInfo,
-    updateLeagueInfo,
-    deleteLeagueInfo
+  getLeagueInfo,
+  setLeagueInfo,
+  updateLeagueInfo,
+  deleteLeagueInfo
 }
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000;
+
 
 const app = express();
 
@@ -9,6 +11,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/ejffl', require('./routes/leagueRoutes'));
 
+app.use(errorHandler);
+
 app.listen(port, () => {
-    console.log(`server start on http://localhost:${port}`);
+  console.log(`server start on http://localhost:${port}`);
 })
